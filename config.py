15dd9b286243f6818adf705b4e3335b57ebf7309
RAVINAMITTAL <ravinamittal04@gmail.com>
2026-06-18 16:10:33 +0530

    add all

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///campus.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "dev-secret")
+    SECRET_KEY = os.getenv(
+        "SECRET_KEY",
+        "dev-secret-key"
+    )
